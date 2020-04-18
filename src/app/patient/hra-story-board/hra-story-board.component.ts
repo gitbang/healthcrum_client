a20b78edbe39c0a21ce87ccb503365daf5a74bdc
heena <heena34bawa@gmail.com>
2020-04-18 17:53:08 +0530

    modified subquestion

--- src/app/patient/hra-story-board/hra-story-board.component.ts
+++ src/app/patient/hra-story-board/hra-story-board.component.ts
@@ -3,14 +3,15 @@ import { Router } from "@angular/router";
 import Swal from "sweetalert2";
 
 // insert a a service
-import { PatientService } from "../patient.service";
+import {PatientService} from  '../patient.service'
 
 @Component({
   selector: "app-hra-story-board",
   templateUrl: "./hra-story-board.component.html",
-  styleUrls: ["./hra-story-board.component.scss"],
+  styleUrls: ["./hra-story-board.component.scss"]
 })
 export class HraStoryBoardComponent implements OnInit {
+  
   /*
   current: number = 1;
   total: number;
@@ -80,149 +81,156 @@ export class HraStoryBoardComponent implements OnInit {
   }  */
   //===============================================================================================//
 
-  constructor(private router: Router, private service: PatientService) {}
+  constructor( private router: Router, private service : PatientService) { }
 
-  category_array = [
-    "Life Style",
-    "Physical Health",
-    "General Health",
-    "Dietary Habits",
-    "Occupational Wellness",
-    "Stress Assessment",
-    "Family History",
-    "Senior Citizen Assessment",
-    "Maternity Risk Assessment",
-  ];
+  category_array = ['Life Style', 'Physical Health','General Health',  'Dietary Habits', 
+                      'Occupational Wellness', 'Stress Assessment','Family History',
+                      'Senior Citizen Assessment' ,'Maternity Risk Assessment']
   category_index = 0;
-  questions: any;
+  questions : any
   ngOnInit() {
-    this.service
-      .getQuestions(this.category_array[this.category_index])
-      .subscribe((questions: any) => {
-        console.log(questions);
-        this.questions = questions;
-        this.organiseQuestions();
-        this.total = this.questions.length;
-      });
+    this.service.getQuestions(this.category_array[this.category_index ]).subscribe((questions : any)=>{
+      console.log(questions);
+      this.questions = questions;
+      this.organiseQuestions();
+      this.total = this.questions.length;
+    })
   }
   currentQuestion = {
-    _id: "", // main question _id
-    mainans: "",
-    mainScore: "",
-    subquestion: [
-      {
-        subAns: "",
-        value: Number,
-      },
-    ],
+    _id : '',           // main question _id
+    mainans : '',
+    mainScore : '',
+    condition : '',
+    subquestion : [{
+        subAns : '',
+        value : Number
+    }]
   };
   index = 0;
-  total;
+  total; 
   subTotal;
   subIndex = 0;
   current = 1;
   control = "main";
-  fromUser = [
-    {
-      questionId: "", // main question _id
-      mainans: "",
-      mainScore: "",
-      subquestion: [
-        {
-          subAns: "",
-          value: "",
-        },
-      ],
-    },
-  ];
-  organiseQuestions() {
+  fromUser = [{
+    questionId : '',           // main question _id
+    mainans : '',
+    mainScore : '',
+    subquestion : [{
+        subAns : '',
+        value : ''        
+    }]
+  }]
+  organiseQuestions () {
+    console.log("organised data reached");
     this.currentQuestion = this.questions[this.index];
   }
 
-  goBack(i) {
-    console.log(i);
-    this.index = this.index - 1;
+  goBack(i){
+    this.index = this.index  - 1; 
     this.current = this.index + 1;
     this.currentQuestion = this.questions[this.index];
-    console.log(this.currentQuestion);
-  }
-
+    this.control = 'main';
+  } 
   selectCondition(value) {
-    if (!this.fromUser[this.index]) {
-      return false;
-    } else if (this.fromUser[this.index].mainans == value) {
-      return true;
-    } else return false;
+    if(this.control == "main") {
+      if(!this.fromUser[this.index]) {
+        return false;
+      }
+      else if(this.fromUser[this.index].mainans == value){
+        return true;
+      }
+      else return false;
+    }
+    else{
+      if(!this.fromUser[this.index].subquestion[this.subIndex]) {
+        return false;
+      }
+      else if(this.fromUser[this.index].subquestion[this.subIndex].subAns == value){
+        return true;
+      }
+      else return false;
+    }
   }
 
-  executeSubQuestion(subans = "", subvalue = 0) {
-    console.log("ans and value are");
-    console.log(subans, subvalue);
+  executeSubQuestion(subans = '', subvalue = 0){
     var temp = {
-      subAns: subans,
-      value: subvalue.toString(),
-    };
+      subAns : subans,
+      value : subvalue.toString()
+    }
     this.fromUser[this.index].subquestion[this.subIndex] = temp;
-    if (this.subIndex >= this.questions[this.index].subQuestionAre.length) {
-    } else if (this.subIndex > 0) {
+   this.subIndex++;
+    if(this.subIndex < this.questions[this.index].subQuestionAre.length) {
+      this.currentQuestion = this.questions[this.index].subQuestionAre[this.subIndex]
+    }
+    else if(this.subIndex > 0){
       this.control = "main";
+      this.subIndex = 0;
       this.normalFlow();
+
     }
   }
 
-  normalFlow() {
-    console.log("contiue");
-    this.index = this.index + 1;
-    this.current = this.index + 1;
-    console.log("index is ", this.index);
-    console.log("question length : ", this.questions.length);
-    if (this.index < this.questions.length) {
+  normalFlow(){
+    this.index = this.index + 1; 
+    this.current = this.index + 1; 
+    console.log("this from user")
+    console.log(this.fromUser);
+    if(this.index <  this.questions.length) {
       this.organiseQuestions();
-    } else if (this.category_index < this.category_array.length) {
-      // save api should add here
+    }
+    else if(this.category_index < this.category_array.length) {
+      console.log("save api add here")
+      this.service.saveAns(this.fromUser).subscribe((response)=>{
+        console.log(response)
+      })
       this.category_index++;
-      this.service
-        .getQuestions(this.category_array[this.category_index])
-        .subscribe((questions: any) => {
-          console.log(questions);
-          this.questions = questions;
-          this.index = 0;
-          this.fromUser.length = 0;
-          this.fromUser = [];
-          this.current = this.index + 1;
-          this.total = this.questions.length;
-          this.organiseQuestions();
-        });
-    } else {
-      this.service.saveAns(this.fromUser).subscribe((response) => {
-        console.log(response);
-      });
+     /* this.service.getQuestions(this.category_array[this.category_index]).subscribe((questions : any)=>{
+        this.questions = questions;
+        this.index = 0; 
+        this.fromUser.length = 0;
+        this.fromUser = [];
+        this.current = this.index + 1;
+        this.total = this.questions.length;
+        this.organiseQuestions();
+      })*/
+    }
+    else{
+       
     }
   }
 
-  async submit(ans, value) {
-    // if(this.control == "subquestion") {
-    //    this.executeSubQuestion(ans, value)
-    // }
-    //else {
-    var temp = {
-      questionId: this.currentQuestion._id,
-      mainans: ans,
-      mainScore: value,
-      subquestion: [],
-    };
-    this.fromUser[this.index] = temp;
-    if (this.questions[this.index].subQuestion == true && false) {
-      this.subTotal = this.questions[this.index].subQuestionAre.length;
-      //this.control = "subquestion";
-      this.currentQuestion = this.questions[this.index].subQuestionAre[
-        this.subIndex
-      ];
-      console.log(this.currentQuestion);
-      //this.executeSubQuestion()
-    } else {
-      this.normalFlow();
+  async submit(ans, value){
+    if(this.control == "subquestion") {
+       this.executeSubQuestion(ans, value) 
     }
-    //}
+    else { 
+      console.log("new question");
+      var temp  = { 
+        questionId : this.currentQuestion._id,
+        mainans : ans,
+        mainScore : value,
+        subquestion : []
+      }
+      console.log("from user");
+      console.log(this.fromUser);
+      if(!this.fromUser[this.index] || this.fromUser.length == 1) {
+        console.log("first times");
+        this.fromUser[this.index] = temp;
+      }
+      else{
+        this.fromUser[this.index].mainans = ans;
+        this.fromUser[this.index].mainScore = value;
+      }
+      console.log("current question" , this.currentQuestion)
+      if(this.questions[this.index].subQuestion == true && this.currentQuestion.condition == ans) {
+        this.subTotal = this.questions[this.index].subQuestionAre.length;
+        this.control = "subquestion";
+        this.currentQuestion = this.questions[this.index].subQuestionAre[this.subIndex];
+      }
+      else {
+        this.normalFlow();
+      }     
+    }
   }
 }
