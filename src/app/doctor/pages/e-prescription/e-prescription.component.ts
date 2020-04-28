e7e8ac64756440044251e39b52646ba0ded771c0
AkashBawa <bawa2027@gmail.com>
2020-04-28 15:29:01 +0530

    E-prescription

--- src/app/doctor/pages/e-prescription/e-prescription.component.ts
+++ src/app/doctor/pages/e-prescription/e-prescription.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import {MatDialog} from "@angular/material"
+import { CommonDashboardComponent } from 'app/shared/common-dashboard/common-dashboard.component';
+
 
 @Component({
   selector: "app-e-prescription",
@@ -6,8 +9,25 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./e-prescription.component.scss"],
 })
 export class EPrescriptionComponent implements OnInit {
-  tabBackground = "primary";
-  constructor() {}
+  //tabBackground = "primary";
+  public showPriscribtion = false;
+  constructor(private dialog : MatDialog) {}
 
   ngOnInit() {}
+
+  showprofile(){
+    console.log("profile reached")
+    const dialogRef  = this.dialog.open(CommonDashboardComponent, {  
+      height : '80%'
+    })
+    
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog box closed`);
+    });
+  }
+  
+  priscription(){
+    console.log("priscription reached")
+    this.showPriscribtion = !this.showPriscribtion
+  }
 }
