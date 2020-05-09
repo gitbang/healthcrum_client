e9eeced3efcaab840319b803e57298209b7a14f0
AkashBawa <bawa2027@gmail.com>
2020-05-09 13:03:32 +0530

    registeration

--- src/app/company/pages/employee-registration/employee-registration.component.ts
+++ src/app/company/pages/employee-registration/employee-registration.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import {DatePipe} from '@angular/common'  ;
-import {MatDialog} from '@angular/material'
+import {MatDialog, MatTableDataSource, MatPaginator} from '@angular/material'
 import {
   FormBuilder,
   FormControl,
@@ -31,6 +31,30 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   }
 }
 
+export interface empdetails {
+  name : string,
+  email : string,
+  contact : number,
+  age : number,
+  empId : string,
+  dept : string,
+  branch : string
+}
+
+const list1 : empdetails[] = [
+  {name : 'Harry', email : "ab@gmail.com", contact : 9874563210, age : 20 , empId : '1234', dept : 'warehouse', branch : 'any' },
+  {name : 'harry1', email : "ab@gmail.com", contact : 9874563210, age : 20 , empId : '1234', dept : 'warehouse', branch : 'any' },
+  {name : 'harry2', email : "ab@gmail.com", contact : 9874563210, age : 20 , empId : '1234', dept : 'warehouse', branch : 'any' },
+  {name : 'harry3', email : "ab@gmail.com", contact : 9874563210, age : 20 , empId : '1234', dept : 'warehouse', branch : 'any' },
+  {name : 'harry4', email : "ab@gmail.com", contact : 9874563210, age : 20 , empId : '1234', dept : 'warehouse', branch : 'any' },
+  {name : 'harry5', email : "ab@gmail.com", contact : 9874563210, age : 20 , empId : '1234', dept : 'warehouse', branch : 'any' },
+  {name : 'harry6', email : "ab@gmail.com", contact : 9874563210, age : 20 , empId : '1234', dept : 'warehouse', branch : 'any' },
+  {name : 'harry7', email : "ab@gmail.com", contact : 9874563210, age : 20 , empId : '1234', dept : 'warehouse', branch : 'any' },
+  {name : 'harry8', email : "ab@gmail.com", contact : 9874563210, age : 20 , empId : '1234', dept : 'warehouse', branch : 'any' },
+  {name : 'harry9', email : "ab@gmail.com", contact : 9874563210, age : 20 , empId : '1234', dept : 'warehouse', branch : 'any' },
+  {name : 'harry10', email : "ab@gmail.com", contact : 9874563210, age : 20 , empId : '1234', dept : 'warehouse', branch : 'any' }
+]
+
 @Component({
   selector: "app-employee-registration",
   templateUrl: "./employee-registration.component.html",
@@ -39,6 +63,8 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 
 export class EmployeeRegistrationComponent implements OnInit {
 
+  @ViewChild(MatPaginator, {static : true}) paginator : MatPaginator
+
   constructor(
     private _formBuilder: FormBuilder, 
     private companyApiService: CompanyApiService,
@@ -46,16 +72,19 @@ export class EmployeeRegistrationComponent implements OnInit {
     private _snackbar : MatSnackBar,
     private service : CompanyService
   ) {}
-
-  ngOnInit() {
-   
-    let dp = new DatePipe(navigator.language);
-    console.log(dp);
-    let p = 'dd-MM-y'; // YYYY-MM-DD
-    let date = dp.transform(new Date(), p);
+  col : string[] = ['name', 'email', 'contact', 'age', 'empId', 'dept', 'branch']
+  list = new MatTableDataSource(list1);
+  
+  ngOnInit() { 
+    setTimeout(() => this.list.paginator = this.paginator)
+    // let dp = new DatePipe(navigator.language);
+    // console.log(dp);
+    // let p = 'dd-MM-y'; // YYYY-MM-DD
+    // //let date = dp.transform(new Date(), p);
 
     this.companyApiService.getEmployeesDetails().subscribe(data => {
       this.empDetails = data;
+      console.log(this.empDetails)
       return;
     });
   }
@@ -65,11 +94,8 @@ export class EmployeeRegistrationComponent implements OnInit {
   genderList: string[] = ["Male", "Female", "other"];
   empDetails: any[];
   page_no = 1;
-  emailFormControl = new FormControl("", [
-    Validators.required,
-    Validators.email
-  ]);
   matcher = new MyErrorStateMatcher();
+ 
   openform(){
     console.log("open form")
     const formport = this.dialog.open(RegisterationFormComponent)
