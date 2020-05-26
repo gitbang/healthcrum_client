d0e4f1dfa1d6b2746c4af1b922fa8975819def57
AkashBawa <bawa2027@gmail.com>
2020-05-26 21:16:50 +0530

    ui

--- src/app/home/pages/blood-test/view-details/view-details.component.ts
+++ src/app/home/pages/blood-test/view-details/view-details.component.ts
@@ -14,10 +14,37 @@ export class ViewDetailsComponent implements OnInit {
     // console.log(data)
     // this.shownresult = data
   }
-  shownresult = {_id : "abc123", name : "Blood Test", includes : "Thyroid Profile-Total (T3, T4 & TSH Ultra-sensitive)", 
+  shownresult = {_id : "abc123", name : "Blood Test", includes : "Thyroid Profile-Total + 73 parameters", 
   reportIn : "24 hrs", totaltest : 12, marketprice : 4200, healcrumprice : 2500, offerprice : 2000}
     
+  profiles = [
+    {
+      name : "Lipid Test", parameters : 74 , 
+      parametersAre : ['Cholesterol-Total','HDL Cholesterol Direct',
+    'LDL Cholesterol -Direct', 'LDL/HDL RATIO', 'HDL / LDL Cholesterol Ratio',
+    'Triglycerides, Serum','Non - HDL Cholesterol, Serum','VLDL']
+    },
+    {
+      name : "Lipid Test", parameters : 74 , 
+      parametersAre : ['Cholesterol-Total','HDL Cholesterol Direct',
+    'LDL Cholesterol -Direct', 'LDL/HDL RATIO', 'HDL / LDL Cholesterol Ratio',
+    'Triglycerides, Serum','Non - HDL Cholesterol, Serum','VLDL']
+    },
+    {
+      name : "Lipid Test", parameters : 74 , 
+      parametersAre : ['Cholesterol-Total','HDL Cholesterol Direct',
+    'LDL Cholesterol -Direct', 'LDL/HDL RATIO', 'HDL / LDL Cholesterol Ratio',
+    'Triglycerides, Serum','Non - HDL Cholesterol, Serum','VLDL']
+    },
+  ]
   ngOnInit() {
   }
 
+  booknow() {
+    console.log("book package")
+  }
+
+  getCall(){
+    console.log("get call")
+  }
 }
